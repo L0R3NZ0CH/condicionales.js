8ba93f637fcf4f159caafb22384c014130a85f37
L0R3NZ0CH <rdzcha77@gmail.com>
2023-10-28 14:40:23 -0600

    terminado

--- app.js
+++ app.js
@@ -76,12 +76,43 @@
 
 // pedir dos numeros y devolder el cociente
 
-let numero1 = prompt ('Escribe un numero');
-let numero2 = prompt ('Escribe otro numero');
+// let numero1 = prompt ('Escribe un numero');
+// let numero2 = prompt ('Escribe otro numero');
 
-if (numero1< 0 && numero2 < 0){
-document.write(' Error 404 ');
-}else if (numero1 > 0 && numero2 > 0){
+// if (numero1 == 0 || numero2 == 0){
+// document.write(' Error 404 ');
+// }else if (numero1 > 0 && numero2 > 0){
 
-    document.write('El cociente es '+ numero1/numero2);
+//     document.write('El cociente es '+ numero1/numero2);
+// }
+
+//  pida dos números y pregunte cuál operación quiere hacer con los números, sumar, restar, multiplicar o dividir.
+let numero1 = prompt('Introduce un numero');
+let numero2 = prompt('Introduce otro numero');
+numero1 = Number(numero1);
+numero2 = Number(numero2);
+
+let operacion = prompt('Que operacion desea realizar  (sumar, restar, multiplicar, o dividir)');
+
+switch (operacion) {
+    case 'sumar':
+       document.write('El resultado de la suma es: '+ (numero1+numero2));
+        
+        break;
+    case 'restar':
+        document.write('El resultado de la resta es: '+(numero1-numero2));
+
+        break;
+    case 'multiplicar':
+        document.write('El resultado de la multiplicacion es:' + (numero1*numero2));
+        
+        break;
+    case 'dividir':
+        document.write('El resultado de la division es: ' + (numero1/numero2));
+
+        break;
+    default:
+        document.write('Escribe una operacion aritmetica');
+        break; 
+        
 }
